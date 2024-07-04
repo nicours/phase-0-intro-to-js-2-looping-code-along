dd73d67e220bae3216b333c7de9080caf47b6033
Nicodemus Kiplangat <nicodemusk84@gmail.com>
2024-07-03 23:47:01 -0400

    Implement writeCards and countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Define the writeCards function
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Define the countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions for testing
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
